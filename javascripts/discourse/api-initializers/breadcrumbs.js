51f6b13a2b4dbe205bf975828bc50f7ce6cfdcb1
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-09-04 10:23:46 +0800

    DEV: Use new plugin outleet

--- javascripts/discourse/api-initializers/breadcrumbs.js
+++ javascripts/discourse/api-initializers/breadcrumbs.js
@@ -1,5 +1,12 @@
 import { apiInitializer } from "discourse/lib/api";
+import BreadCrumbs from "../components/breadcrumbs";
 
-export default apiInitializer("0.11.1", api => {
-  console.log("hello world from api initializer!");
+export default apiInitializer("1.9.0", (api) => {
+  [
+    "above-category-navigation",
+    "above-default-navigation",
+    "above-topic-container",
+  ].forEach((outletName) => {
+    api.renderInPluginOutlet(outletName, BreadCrumbs);
+  });
 });
--- javascripts/discourse/components/breadcrumbs.js
+++ javascripts/discourse/components/breadcrumbs.js
@@ -2,15 +2,19 @@ import Component from "@glimmer/component";
 import Category from "discourse/models/category";
 
 export default class extends Component {
+  get topic() {
+    return this.args.outletArgs.topic;
+  }
+
   get category() {
-    if (this.args.category) {
+    if (this.args.outletArgs.category) {
       return this.args.category;
-    } else if (this.args.topic) {
-      return this.args.topic.category;
+    } else if (this.topic) {
+      return this.topic.category;
     }
   }
 
   get categoryModel() {
     return `${Category.slugFor(this.category)}/${this.category.id}`;
   }
-};
+}
